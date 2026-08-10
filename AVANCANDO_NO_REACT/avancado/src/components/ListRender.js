4f2a7fc5452165889550f719bc0582438cc8f223
Arthur Albuquerque <106913775+artjiyr@users.noreply.github.com>
2026-08-09 22:56:53 -0300

    Previous State

--- AVANCANDO_NO_REACT/avancado/src/components/ListRender.js
+++ AVANCANDO_NO_REACT/avancado/src/components/ListRender.js
@@ -3,13 +3,35 @@ import { useState } from "react";
 const ListRender = () => {
     const [list] = useState(["Mari", "Arthur", "Pedro"]);
 
+    const[users, setUsers] = useState([
+        {id: 1, name: "Mari Miya", age: 11},
+        {id: 2, name: "Hinsuki", age: 12},
+        {id: 3, name: "Tadash", age: 12},
+    ])
+
+    const DeleteRandom = () => {
+        const RandomNumber = Math.floor(Math.random() * 4);
+
+        setUsers((prevUsers) => {
+            console.log(prevUsers)
+            return prevUsers.filter((user) => RandomNumber !== user.id)
+        })
+    }
+
     return(
         <div>
             <ul>
-                {list.map((item) => (
-                    <li>{item}</li>
+                {list.map((item, i) => (
+                    <li key={i}>{item}</li>
                 ))}
             </ul>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name} - {user.age}
+                    </li>
+                ))}
+            </ul>
+            <button onClick={DeleteRandom}>Delete random user</button>
         </div>
     )
 }
